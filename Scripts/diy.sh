2916241dfc1ddb62c3ce6ba73f06772a2fee6729
zongheng23333 <1954640276@qq.com>
2024-12-18 20:29:22 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-
+git clone   -b  main  --single-branch  https://github.com/lmq8267/luci-app-vnt    package/zongheng/luci-app-vnt 
+git clone   -b  main  --single-branch   https://github.com/vnt-dev/vnt    package/zongheng/vnt  
 ./scripts/feeds update -a 
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
